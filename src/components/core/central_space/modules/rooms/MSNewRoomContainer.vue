02c95f83a22ef0fde7af0c6b58487db22cbcfa95
zRain <2296342883@qq.com>
2023-04-21 00:01:36 +0800

    feat: 🎸 添加新建room基本文件结构

--- src/components/core/central_space/modules/rooms/MSNewRoomContainer.vue
+++ src/components/core/central_space/modules/rooms/MSNewRoomContainer.vue
@@ -1,7 +1,41 @@
 <template>
-  <div class="MSNewRoomContainer">New Room</div>
+  <div class="MSNewRoomContainer">
+    <!-- <div class="Banner">
+      Meeting your new friends <br />
+      in a new room<span>.</span>
+    </div> -->
+  </div>
 </template>
 
 <script setup lang="ts"></script>
 
-<style lang="scss"></style>
+<style lang="scss">
+.MSNewRoomContainer {
+  position: relative;
+  display: flex;
+  flex-direction: column;
+
+  .Banner {
+    text-align: center;
+    font-size: 2rem;
+    font-family: var(--f-rb);
+
+    span {
+      color: var(--c-green);
+    }
+  }
+  /* &::after {
+    position: absolute;
+    content: '';
+    left: 0;
+    top: -20px;
+    height: 200px;
+    width: 100%;
+    background-image: url('/illustrations/new_room_bg_1.svg');
+    background-repeat: no-repeat;
+    background-size: contain;
+    background-position: center;
+    z-index: -1;
+  } */
+}
+</style>
